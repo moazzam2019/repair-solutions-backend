63cd87281e711072e62671d752c584c3271d58d7
moazzam2019 <moazzam.iqbal2019@gmail.com>
2022-11-21 13:21:10 +0500

    mongoose v6 added

--- server.js
+++ server.js
@@ -9,13 +9,21 @@ const DB = process.env.DATABASE.replace(
   process.env.DATABASE_PASSWORD
 );
 
-mongoose
-  .connect(DB, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  })
-  .then(() => console.log("DB connection successful!"));
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect(DB);
+  console.log("DB connection successful V6");
+}
+
+// For Mongoose V5
+// mongoose
+//   .connect(DB, {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false,
+//   })
+//   .then(() => console.log("DB connection successful!"));
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
